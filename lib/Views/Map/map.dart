fa6d6ee0f6854dd23cd1f33eeb6e85b05e5e77d0
Simo <simone.ghiazzi01@universitadipavia.it>
2021-05-01 19:25:42 +0200

    setup geolocator plugin

--- lib/Views/Map/map.dart
+++ lib/Views/Map/map.dart
@@ -2,28 +2,63 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+import 'package:geolocator/geolocator.dart';
+
 class Map extends StatefulWidget {
   @override
-  State<Map> createState() => MapState();
+  MapState createState() => MapState();
 }
 
 class MapState extends State<Map> {
+  Geolocator geolocator = Geolocator();
+  Position userLocation;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocation().then((position) {
+      userLocation = position;
+    });
+  }
+
   Completer<GoogleMapController> _controller = Completer();
-  static final LatLng colosseoLatLng = LatLng(41.890251, 12.492373);
-  static final CameraPosition colosseo = CameraPosition(
-    target: LatLng(41.890251, 12.492373),
-    zoom: 16.5,
-  );
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.normal,
-        initialCameraPosition: colosseo,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
+      body: userLocation == null
+          ? Center(
+              child: ElevatedButton(
+                  onPressed: () {
+                    _getLocation().then((value) {
+                      setState(() {
+                        userLocation = value;
+                      });
+                    });
+                  },
+                  child: Text('Position')),
+            )
+          : GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: CameraPosition(
+                target: LatLng(userLocation.latitude, userLocation.longitude),
+                zoom: 16,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
     );
+  }
+
+  Future<Position> _getLocation() async {
+    var currentLocation;
+    try {
+      currentLocation = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.best);
+    } catch (e) {
+      currentLocation = null;
+    }
+    return currentLocation;
   }
 }
