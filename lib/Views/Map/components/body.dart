2eefb5cf22958f7840683c6070e5bef572e2d025
Simo <simone.ghiazzi01@universitadipavia.it>
2021-05-08 12:18:55 +0200

    Search bar map

--- lib/Views/Map/components/body.dart
+++ lib/Views/Map/components/body.dart
@@ -26,25 +26,54 @@ class _BodyState extends State<Body> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: StreamBuilder<Position>(
-            stream: widget.mapViewModel.position,
-            builder: (context, snapshot) {
-              return snapshot.data == null
-                  ? CircularProgressIndicator()
-                  : GoogleMap(
-                      mapType: MapType.normal,
-                      initialCameraPosition: CameraPosition(
-                        target: LatLng(
-                            snapshot.data.latitude, snapshot.data.longitude),
-                        zoom: 16,
-                      ),
-                      myLocationButtonEnabled: true,
-                      myLocationEnabled: true,
-                      onMapCreated: (GoogleMapController controller) {
-                        widget.mapViewModel.mapController.complete(controller);
-                      });
-            }));
+    return Stack(children: <Widget>[
+      Center(
+          child: StreamBuilder<Position>(
+              stream: widget.mapViewModel.position,
+              builder: (context, snapshot) {
+                return snapshot.data == null
+                    ? CircularProgressIndicator()
+                    : GoogleMap(
+                        mapType: MapType.normal,
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(
+                              snapshot.data.latitude, snapshot.data.longitude),
+                          zoom: 16,
+                        ),
+                        myLocationButtonEnabled: true,
+                        myLocationEnabled: true,
+                        onMapCreated: (GoogleMapController controller) {
+                          widget.mapViewModel.mapController
+                              .complete(controller);
+                        });
+              })),
+      Positioned(
+          top: 60,
+          right: 15,
+          left: 15,
+          child: Container(
+              color: Colors.white,
+              child: Row(children: <Widget>[
+                IconButton(
+                  splashColor: Colors.grey,
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                Expanded(
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.go,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                        hintText: "Search..."),
+                  ),
+                ),
+              ])))
+    ]);
   }
 
   /*StreamSubscription<Position> subscribeToViewModel() {
