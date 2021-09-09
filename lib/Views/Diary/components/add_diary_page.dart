934b47b7de25a92d08c21918dbc5db7c9ca5c018
Simo <simone.ghiazzi01@universitadipavia.it>
2021-09-09 17:55:52 +0200

    diary ad page

--- lib/Views/Diary/components/add_diary_page.dart
+++ lib/Views/Diary/components/add_diary_page.dart
@@ -1,13 +1,11 @@
 /*import 'package:dear_diary/view_model/base.dart';
 import 'package:dear_diary/view_model/entry.dart';*/
-import 'package:dima_colombo_ghiazzi/Router/app_router_delegate.dart';
 import 'package:dima_colombo_ghiazzi/ViewModel/BaseUser/diary_view_model.dart';
 import 'package:dima_colombo_ghiazzi/Views/Chat/components/top_bar.dart';
+import 'package:dima_colombo_ghiazzi/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
-
-import '../../../constants.dart';
 
 //import '../home.dart';
 
@@ -22,62 +20,77 @@ class AddEntry extends StatefulWidget {
 class _AddEntryState extends State<AddEntry> {
   Map<String, String> _formData = {};
   final _addEntryFormKey = GlobalKey<FormState>();
-  AppRouterDelegate routerDelegate;
-
-  @override
-  void initState() {
-    routerDelegate = Provider.of<AppRouterDelegate>(context, listen: false);
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          TopBar(
-            text: 'New page',
-          ),
-          Form(
-            key: _addEntryFormKey,
-            child: Column(
+      body: SafeArea(
+        child: Stack(
+          children: <Widget>[
+            TopBar(
+              text: 'New page',
+            ),
+            ListView(
               children: <Widget>[
-                Padding(
-                  padding: EdgeInsets.only(bottom: 20.0),
-                  child: TextFormField(
-                    keyboardType: TextInputType.multiline,
-                    minLines: 1,
-                    maxLines: 3,
-                    cursorColor: Color(0xFF3C4858),
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: 'What\'s our topic of discussion?',
-                    ),
-                    inputFormatters: [
-                      LengthLimitingTextInputFormatter(100),
-                    ],
-                    //validator: DiaryViewModel.title,
-                    onSaved: (value) => _formData['title'] = value,
+                Theme(
+                  data: ThemeData(
+                    highlightColor: Colors.transparent,
+                    splashColor: Colors.transparent,
+                    inputDecorationTheme:
+                        InputDecorationTheme(border: InputBorder.none),
                   ),
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.multiline,
-                  maxLines: null,
-                  cursorColor: Color(0xFF3C4858),
-                  decoration: InputDecoration.collapsed(
-                      hintText: 'Tell me about it...'),
-                  //validator: DiaryViewModel.content,
-                  onSaved: (value) => _formData['content'] = value,
+                  child: Container(
+                      padding: EdgeInsets.only(
+                        top: 30,
+                        left: size.width * 0.1,
+                        right: size.width * 0.1,
+                        bottom: 10,
+                      ),
+                      child: Form(
+                        key: _addEntryFormKey,
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 45),
+                            Padding(
+                              padding: EdgeInsets.only(bottom: 20.0),
+                              child: TextFormField(
+                                keyboardType: TextInputType.multiline,
+                                minLines: 1,
+                                maxLines: 3,
+                                cursorColor: Color(0xFF3C4858),
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                                decoration: InputDecoration(
+                                  hintText: 'What\'s our topic of discussion?',
+                                ),
+                                inputFormatters: [
+                                  LengthLimitingTextInputFormatter(100),
+                                ],
+                                //validator: DiaryViewModel.title,
+                                onSaved: (value) => _formData['title'] = value,
+                              ),
+                            ),
+                            TextFormField(
+                              keyboardType: TextInputType.multiline,
+                              maxLines: null,
+                              cursorColor: Color(0xFF3C4858),
+                              decoration: InputDecoration.collapsed(
+                                  hintText: 'Tell me about it...'),
+                              //validator: DiaryViewModel.content,
+                              onSaved: (value) => _formData['content'] = value,
+                            ),
+                          ],
+                        ),
+                      )),
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xFF3C4858),
+        backgroundColor: kPrimaryColor,
         child:
             /*Provider.of<EntryViewModel>(context).viewStatus ==
                 ViewStatus.Loading
