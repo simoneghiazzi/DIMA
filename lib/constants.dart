01f4c1284fabc70b32e3c81ec2c8f6a15084a2a0
simoneghiazzi <48355703+simoneghiazzi@users.noreply.github.com>
2021-11-18 17:10:55 +0100

    Calendar graphics

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,5 +2,7 @@ import 'package:flutter/material.dart';
 
 const kPrimaryColor = Color(0xFF212C8C);
 const kPrimaryLightColor = Color(0xFFC9E9FF);
+final kPrimaryMediumColor = Colors.blue.shade600;
+final kPrimaryGoldenColor = Colors.amber.shade600;
 final greyColor = Color(0xffaeaeae);
 final lightGreyColor = Color(0xffE8E8E8);
