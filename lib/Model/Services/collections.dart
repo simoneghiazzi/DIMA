e0a8b8e569b4fbe90f2720e49e64b16421fde8fd
lucacolombo97 <luca.colombo97@libero.it>
2021-12-10 14:48:26 +0100

    collection refactor + added methods for getting userviewmodel and home page route

--- lib/Model/Services/collections.dart
+++ lib/Model/Services/collections.dart
@@ -1,3 +1,10 @@
+import 'package:sApport/ViewModel/user_view_model.dart';
+import 'package:sApport/ViewModel/Expert/expert_view_model.dart';
+import 'package:sApport/ViewModel/BaseUser/base_user_view_model.dart';
+import 'package:sApport/Views/Home/BaseUser/base_user_home_page_screen.dart';
+import 'package:sApport/Views/Home/Expert/expert_home_page_screen.dart';
+
+// DB Collections
 enum Collection {
   BASE_USERS,
   EXPERTS,
@@ -8,44 +15,75 @@ enum Collection {
   PENDING_CHATS,
   REQUESTS_CHATS,
   EXPERT_CHATS,
-  DIARY
+  DIARY,
 }
 
 extension Utils on Collection {
+  /// Returns the name of the collection saved into the DB
   String get value {
     switch (this) {
       case Collection.BASE_USERS:
-        return 'users';
+        return "users";
         break;
       case Collection.EXPERTS:
-        return 'experts';
+        return "experts";
         break;
       case Collection.REPORTS:
-        return 'reports';
+        return "reports";
         break;
       case Collection.MESSAGES:
-        return 'messages';
+        return "messages";
         break;
       case Collection.UTILS:
-        return 'utils';
+        return "utils";
         break;
       case Collection.ACTIVE_CHATS:
-        return 'anonymousActiveChats';
+        return "anonymousActiveChats";
         break;
       case Collection.PENDING_CHATS:
-        return 'anonymousPendingChats';
+        return "anonymousPendingChats";
         break;
       case Collection.REQUESTS_CHATS:
-        return 'anonymousRequestChats';
+        return "anonymousRequestChats";
         break;
       case Collection.EXPERT_CHATS:
-        return 'expertChats';
+        return "expertChats";
         break;
       case Collection.DIARY:
-        return 'diary';
+        return "diary";
         break;
       default:
-        return '';
+        return "";
+        break;
+    }
+  }
+
+  /// Returns the [UserViewModel] of the relative user collections
+  UserViewModel get userViewModel {
+    switch (this) {
+      case Collection.BASE_USERS:
+        return BaseUserViewModel();
+        break;
+      case Collection.EXPERTS:
+        return ExpertViewModel();
+        break;
+      default:
+        return null;
+        break;
+    }
+  }
+
+  /// Returns the home page route of the relative user collections
+  String get homePageRoute {
+    switch (this) {
+      case Collection.BASE_USERS:
+        return BaseUserHomePageScreen.route;
+        break;
+      case Collection.EXPERTS:
+        return ExpertHomePageScreen.route;
+        break;
+      default:
+        return "";
         break;
     }
   }
