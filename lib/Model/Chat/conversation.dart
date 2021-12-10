4c7ef61098f425da2e7248b1592226f1c4a59baa
lucacolombo97 <luca.colombo97@libero.it>
2021-12-10 23:41:25 +0100

    conversation refactor

--- lib/Model/Chat/conversation.dart
+++ lib/Model/Chat/conversation.dart
@@ -1,25 +1,30 @@
-import 'package:sApport/Model/Chat/chat.dart';
 import 'package:sApport/Model/user.dart';
+import 'package:sApport/Model/Chat/chat.dart';
 
 class Conversation {
-  User senderUser, peerUser;
-  Chat senderUserChat, peerUserChat;
+  // Users
+  User senderUser;
+  User peerUser;
+
+  // Chats
+  Chat senderUserChat;
+  Chat peerUserChat;
+
+  // It represents the composed id of the 2 users
   String pairChatId;
 
   /// It represents the conversation between a [senderUser] and a [peerUser].
-  /// 
-  /// It receives also the type of chat of the sender user as the [senderUserChat] 
+  ///
+  /// It receives also the type of chat of the sender user as the [senderUserChat]
   /// and the type of chat of the peer user as the [peerUserChat].
   Conversation({this.senderUser, this.senderUserChat, this.peerUser, this.peerUserChat});
 
-  /// Compute the pair chat id between 2 users
+  /// Compute the pair chat id of the 2 users involved in the conversation
   void computePairChatId() {
-    var senderId = senderUser.id;
-    var peerId = peerUser.id;
     if (senderUser.id.hashCode <= peerUser.id.hashCode) {
-      pairChatId = '$senderId-$peerId';
+      pairChatId = "${senderUser.id}-${peerUser.id}";
     } else {
-      pairChatId = '$peerId-$senderId';
+      pairChatId = "${peerUser.id}-${senderUser.id}";
     }
   }
 }
