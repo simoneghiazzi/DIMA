cb37b855e63bb4bad99b3ee4299cce3cc89dd3f9
lucacolombo97 <luca.colombo97@libero.it>
2021-12-11 01:41:09 +0100

    user view model refactor

--- lib/ViewModel/user_view_model.dart
+++ lib/ViewModel/user_view_model.dart
@@ -1,15 +1,29 @@
 import 'package:get_it/get_it.dart';
+import 'package:sApport/Model/Services/firebase_auth_service.dart';
 import 'package:sApport/Model/user.dart';
+import 'package:sApport/Model/Services/collections.dart';
 import 'package:sApport/Model/Services/firestore_service.dart';
 import 'package:sApport/ViewModel/Forms/base_user_signup_form.dart';
 
-abstract class UserViewModel {
-  final FirestoreService firestoreService = GetIt.I<FirestoreService>();
-  User loggedUser;
-  
-  /// It takes the [id] and return the logged User from the DB
-  Future<void> loadLoggedUser(String id);
+class UserViewModel {
+  // Services
+  final FirestoreService _firestoreService = GetIt.I<FirestoreService>();
+  final FirebaseAuthService _firebaseAuthService = GetIt.I<FirebaseAuthService>();
 
-  /// Create a new user with email and password with the [infoViewModel] info
-  User createUser(BaseUserSignUpForm infoViewModel);
+  User loggedUser;
+
+  /// It takes the [collection] and instantiate the [loggedUser]
+  /// with all the retrieved information from the DB
+  Future<void> loadLoggedUser(Collection collection) {
+    loggedUser = collection.userModel;
+    return _firestoreService
+        .getUserByIdFromDB(collection, _firebaseAuthService.userCredential.user.uid)
+        .then((value) => loggedUser.setFromDocument(value.docs[0]));
+  }
+
+  /// Create a new user from the [baseUserSignUpForm] info
+  void createUser(Collection collection, BaseUserSignUpForm baseUserSignUpForm) {
+    loggedUser = collection.userModel;
+    loggedUser.setFromSignUpForm(baseUserSignUpForm);
+  }
 }
