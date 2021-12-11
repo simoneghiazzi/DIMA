1190ace6b3b4d0484dc842909b6a4756fd4b83da
lucacolombo97 <luca.colombo97@libero.it>
2021-12-11 01:16:11 +0100

    db item refactor

--- lib/Model/db_item.dart
+++ lib/Model/db_item.dart
@@ -2,7 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sApport/Model/Services/collections.dart';
 
 abstract class DbItem {
+  /// Get the data of the DBItem as a key-value map
+  Map getData();
+
+  /// Set the fields of the DBItem from the [doc]
   void setFromDocument(DocumentSnapshot doc);
-  getData();
+
+  /// Collection of the DBItem
   Collection get collection;
 }
