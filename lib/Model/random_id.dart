57fc2fce5ddab5d52a18b782b772927a1c32e9f2
lucacolombo97 <luca.colombo97@libero.it>
2021-12-11 01:20:17 +0100

    random id refactor

--- lib/Model/random_id.dart
+++ lib/Model/random_id.dart
@@ -1,16 +1,15 @@
 import 'dart:math';
 
 class RandomId {
+  // It represents the alphabet of all the permitted alphanumeric values of the id into FireBase
   static const String AUTO_ID_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  // Generate a random ID for retrieving a random user from the DB
+  /// Generate a random ID of the specified [idLength]
   static String generate({int idLength = 28}) {
-    final rand = new Random.secure();
-    StringBuffer builder = new StringBuffer();
-    int maxRandom = AUTO_ID_ALPHABET.length;
+    StringBuffer idBuilder = new StringBuffer();
     for (int i = 0; i < idLength; i++) {
-      builder.writeCharCode(AUTO_ID_ALPHABET.codeUnitAt((rand.nextInt(maxRandom))));
+      idBuilder.writeCharCode(AUTO_ID_ALPHABET.codeUnitAt((Random.secure().nextInt(AUTO_ID_ALPHABET.length))));
     }
-    return builder.toString();
+    return idBuilder.toString();
   }
 }
