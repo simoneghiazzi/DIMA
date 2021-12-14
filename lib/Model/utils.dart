c9cff1d099da21776cca11bce35a1cd3c607d560
lucacolombo97 <luca.colombo97@libero.it>
2021-12-14 02:48:27 +0100

    utils

--- lib/Model/utils.dart
+++ lib/Model/utils.dart
@@ -1,15 +1,24 @@
 import 'dart:math';
 
-class RandomId {
+class Utils {
   // It represents the alphabet of all the permitted alphanumeric values of the id into FireBase
   static const String AUTO_ID_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  /// Generate a random ID of the specified [idLength]
-  static String generate({int idLength = 28}) {
+  /// Generate a random ID of the specified [idLength].
+  static String randomId({int idLength = 28}) {
     StringBuffer idBuilder = new StringBuffer();
     for (int i = 0; i < idLength; i++) {
       idBuilder.writeCharCode(AUTO_ID_ALPHABET.codeUnitAt((Random.secure().nextInt(AUTO_ID_ALPHABET.length))));
     }
     return idBuilder.toString();
+  }
+
+  /// Returns the pair chat id of 2 users based on the [senderId] and the [peerId].
+  static String pairChatId(String senderId, String peerId) {
+    if (senderId.hashCode <= peerId.hashCode) {
+      return "$senderId-$peerId";
+    } else {
+      return "$peerId-$senderId";
+    }
   }
 }
