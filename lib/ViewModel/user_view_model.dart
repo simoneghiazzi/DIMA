6591de146cf74a47f34105fb6f12fd7c421b0abb
lucacolombo97 <luca.colombo97@libero.it>
2021-12-14 02:48:02 +0100

    user service

--- lib/ViewModel/user_view_model.dart
+++ lib/ViewModel/user_view_model.dart
@@ -1,29 +1,24 @@
 import 'package:get_it/get_it.dart';
+import 'package:sApport/Model/Services/user_service.dart';
 import 'package:sApport/Model/user.dart';
-import 'package:sApport/Model/Services/firestore_service.dart';
-import 'package:sApport/Model/Services/firebase_auth_service.dart';
 import 'package:sApport/ViewModel/Forms/base_user_signup_form.dart';
 
 class UserViewModel {
   // Services
-  final FirestoreService _firestoreService = GetIt.I<FirestoreService>();
-  final FirebaseAuthService _firebaseAuthService = GetIt.I<FirebaseAuthService>();
+  final UserService _userService = GetIt.I<UserService>();
 
-  User loggedUser;
-
-  /// It instantiate the [loggedUser] with all the retrieved information from the DB.
+  /// It load the signed in user with all the retrieved information from the DB.
   ///
   /// If the user is not signed in, it throws an exception.
-  Future<void> loadLoggedUser() async {
-    assert(_firebaseAuthService.firebaseUser != null);
-    loggedUser = await _firestoreService.findUserType(_firebaseAuthService.firebaseUser.uid);
-    return _firestoreService
-        .getUserByIdFromDB(loggedUser.collection, _firebaseAuthService.firebaseUser.uid)
-        .then((value) => loggedUser.setFromDocument(value.docs[0]));
+  Future<void> loadUser() {
+    return _userService.loadLoggedUserFromDB();
   }
 
-  /// Create a new user based on [collection] with the [baseUserSignUpForm] info.
+  /// Create a new user from the [baseUserSignUpForm] info.
   void createUser(BaseUserSignUpForm baseUserSignUpForm) {
-    loggedUser = baseUserSignUpForm.user;
+    return _userService.createUserFromSignUpForm(baseUserSignUpForm);
   }
+
+  /// Returns the currently logged user.
+  User get loggedUser => _userService.loggedUser;
 }
