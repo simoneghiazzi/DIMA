583ee8d5e9ebb2ad8536ddcc580357b3d3a1eab4
lucacolombo97 <luca.colombo97@libero.it>
2021-12-15 13:42:54 +0100

    empty landscape body

---
+++ lib/Views/components/empty_landscape_body.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:sApport/Views/components/top_bar.dart';
+import 'package:sApport/constants.dart';
+
+class EmptyLandscapeBody extends StatelessWidget {
+  const EmptyLandscapeBody({Key key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        TopBar(
+          text: "",
+          isPortrait: true,
+        ),
+        Expanded(
+          child: Container(
+            color: kPrimaryLightGreyColor,
+            child: Center(
+              child: Image.asset(
+                "assets/icons/logo.png",
+                height: size.height / 10,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
