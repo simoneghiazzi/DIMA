64298e115e346c353cd66773ef1f57d1af373db1
lucacolombo97 <luca.colombo97@libero.it>
2021-12-25 02:53:59 +0100

    profile views refactor

--- lib/Views/Profile/expert_profile_screen.dart
+++ lib/Views/Profile/expert_profile_screen.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'components/expert_profile_body.dart';
 import 'package:sApport/Model/DBItems/Expert/expert.dart';
 
+/// It contains the [ExpertProfileBody] that is used to show the base information
+/// of the [expert] and the button for opening a chat a new chat with him/her.
 class ExpertProfileScreen extends StatelessWidget {
-  static const route = '/expertProfileScreen';
-  final Expert? expert;
+  /// Route of the page used by the Navigator.
+  static const route = "/expertProfileScreen";
+  final Expert expert;
 
-  ExpertProfileScreen({Key? key, required this.expert}) : super(key: key);
+  /// It contains the [ExpertProfileBody] that is used to show the base information
+  /// of the [expert] and the button for opening a chat a new chat with him/her.
+  const ExpertProfileScreen({Key? key, required this.expert}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ExpertProfileBody(
-        expert: expert,
-      ),
+      body: ExpertProfileBody(expert: expert),
     );
   }
 }
