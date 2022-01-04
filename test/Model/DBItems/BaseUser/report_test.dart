7ac78875473ac43f00922c7ecc1ca5a206d65537
lucacolombo97 <luca.colombo97@libero.it>
2022-01-04 20:44:15 +0100

    report model test

---
+++ test/Model/DBItems/BaseUser/report_test.dart
@@ -0,0 +1,52 @@
+import 'package:test/test.dart';
+import 'package:sApport/Model/utils.dart';
+import 'package:sApport/Model/DBItems/BaseUser/report.dart';
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+
+void main() async {
+  /// Fake Firebase
+  final fakeFirebase = FakeFirebaseFirestore();
+
+  /// Mock Fields
+  var id = Utils.randomId();
+  var category = "Report category";
+  var description = "Report description";
+  var dateTime = DateTime(2021, 10, 19);
+
+  /// Mock DiaryPage
+  Report mockReport = Report(
+    id: id,
+    category: category,
+    description: description,
+    dateTime: dateTime,
+  );
+
+  /// Add the mock diary page to the fakeFirebase
+  fakeFirebase.collection(mockReport.collection).doc(mockReport.id).set(mockReport.data);
+
+  group("Report initialization", () {
+    test("Report collection", () {
+      expect(mockReport.collection, Report.COLLECTION);
+    });
+  });
+
+  group("Report data", () {
+    test("Set report from document", () async {
+      var result = (await fakeFirebase.collection(mockReport.collection).doc(mockReport.id).get());
+      var retrievedReport = Report.fromDocument(result);
+      expect(retrievedReport.id, id);
+      expect(retrievedReport.category, category);
+      expect(retrievedReport.description, description);
+      expect(retrievedReport.dateTime, dateTime);
+    });
+
+    test("Get report data as a key-value map", () async {
+      expect(mockReport.data, {
+        "id": id,
+        "category": category,
+        "description": description,
+        "dateTime": dateTime,
+      });
+    });
+  });
+}
