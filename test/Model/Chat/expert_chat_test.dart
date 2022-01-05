f6adc2685b0aa987e3a8214c50dca99aa71d9bfe
lucacolombo97 <luca.colombo97@libero.it>
2022-01-05 21:57:52 +0100

    expert chat test

--- test/Model/Chat/expert_chat_test.dart
+++ test/Model/Chat/expert_chat_test.dart
@@ -2,7 +2,8 @@ import 'package:test/test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:sApport/Model/utils.dart';
-import 'package:sApport/Model/Chat/anonymous_chat.dart';
+import 'package:sApport/Model/Chat/expert_chat.dart';
+import 'package:sApport/Model/DBItems/Expert/expert.dart';
 import 'package:sApport/Model/Services/user_service.dart';
 import 'package:sApport/Model/DBItems/BaseUser/base_user.dart';
 import 'package:sApport/Model/Services/firestore_service.dart';
@@ -19,20 +20,20 @@ void main() async {
 
   /// Mock Fields
   var userId = Utils.randomId();
-  var peerUser = BaseUser(id: Utils.randomId());
+  var peerUser = Expert(id: Utils.randomId());
 
   var lastMessage = "Last message test";
   var lastMessageDateTime = DateTime(2021, 10, 19, 21, 10, 50);
   var notReadMessages = 4;
 
-  /// Mock AnonymousChat
-  AnonymousChat mockAnonymousChat = AnonymousChat(
+  /// Mock ExpertChat
+  ExpertChat mockAnonymousChat = ExpertChat(
     lastMessage: lastMessage,
     lastMessageDateTime: lastMessageDateTime,
     notReadMessages: notReadMessages,
   );
 
-  /// Add the mock anonymous chat to the fakeFirebase
+  /// Add the mock expert chat to the fakeFirebase
   fakeFirebase
       .collection(BaseUser.COLLECTION)
       .doc(userId)
@@ -40,31 +41,31 @@ void main() async {
       .doc(peerUser.id)
       .set({"lastMessageTimestamp": lastMessageDateTime.millisecondsSinceEpoch, "notReadMessages": notReadMessages, "lastMessage": lastMessage});
 
-  group("AnonymousChat initialization", () {
-    var anonymousChatTest = AnonymousChat();
+  group("ExpertChat initialization", () {
+    var expertChatTest = ExpertChat();
 
-    test("Anonymous chat collection", () {
-      expect(anonymousChatTest.collection, AnonymousChat.COLLECTION);
+    test("Expert chat collection", () {
+      expect(expertChatTest.collection, ExpertChat.COLLECTION);
     });
 
-    test("Anonymous chat peer user collection", () {
-      expect(anonymousChatTest.peerCollection, AnonymousChat.PEER_COLLECTION);
+    test("Expert chat peer user collection", () {
+      expect(expertChatTest.peerCollection, ExpertChat.PEER_COLLECTION);
     });
 
-    test("Anonymous chat notReadMessages initially set to 0", () {
-      expect(anonymousChatTest.notReadMessages, 0);
+    test("Expert chat notReadMessages initially set to 0", () {
+      expect(expertChatTest.notReadMessages, 0);
     });
   });
 
-  group("AnonymousChat data", () {
-    test("Anonymous chat factory from document", () async {
+  group("ExpertChat data", () {
+    test("Expert chat factory from document", () async {
       var result = (await fakeFirebase.collection(BaseUser.COLLECTION).doc(userId).collection(mockAnonymousChat.collection).doc(peerUser.id).get());
-      var retrievedAnonymousChat = AnonymousChat.fromDocument(result);
+      var retrievedExpertChat = ExpertChat.fromDocument(result);
 
-      expect(retrievedAnonymousChat.lastMessage, lastMessage);
-      expect(retrievedAnonymousChat.lastMessageDateTime, lastMessageDateTime);
-      expect(retrievedAnonymousChat.notReadMessages, notReadMessages);
-      expect(retrievedAnonymousChat.peerUser!.id, peerUser.id);
+      expect(retrievedExpertChat.lastMessage, lastMessage);
+      expect(retrievedExpertChat.lastMessageDateTime, lastMessageDateTime);
+      expect(retrievedExpertChat.notReadMessages, notReadMessages);
+      expect(retrievedExpertChat.peerUser!.id, peerUser.id);
     });
   });
 }
